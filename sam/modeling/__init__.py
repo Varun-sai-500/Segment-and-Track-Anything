eb7c2d54330a3e4f930e22d352c48f305a554595
Varun Sai <varunsaip2005@gmail.com>
2026-08-02 11:36:43 +0530

    refactor: remove training parts and keep inference only

---
+++ sam/modeling/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from sam.modeling.sam import Sam
+from sam.modeling.image_encoder import ImageEncoderViT
+from sam.modeling.mask_decoder import MaskDecoder
+from sam.modeling.prompt_encoder import PromptEncoder
+from sam.modeling.transformer import TwoWayTransformer
