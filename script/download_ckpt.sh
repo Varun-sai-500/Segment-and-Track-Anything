018da60c1d926dc738e82cb785f1fee97bbd016d
Varun Sai <varunsaip2005@gmail.com>
2026-08-21 10:56:12 +0530

    remove shell script

--- script/download_ckpt.sh
+++
@@ -1,7 +0,0 @@
-mkdir ckpt
-
-# download aot-ckpt
-gdown '1QoChMkTVxdYZ_eBlZhK2acq9KMQZccPJ' --output ./ckpt/R50_DeAOTL_PRE_YTB_DAV.pth
-
-curl -L https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth \
-  -o ./ckpt/sam_vit_b_01ec64.pth
